f087b660d27c25e487c018f0245d294155376ae0
Edras <edrasalvarezoliva@gmail.com>
2023-03-18 20:03:29 -0600

    V2 actualizacion call and share

--- app/src/main/java/com/cdp/TL01E171/VerActivity.java
+++ app/src/main/java/com/cdp/TL01E171/VerActivity.java
@@ -2,6 +2,7 @@ package com.cdp.TL01E171;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -23,7 +24,7 @@ public class VerActivity extends AppCompatActivity {
     EditText txtNombre, txtTelefono, txtNota;
     Spinner txtPais,opciones;
     Button btnGuarda;
-    FloatingActionButton fabEditar, fabEliminar;
+    FloatingActionButton fabEditar, fabEliminar, fabCompartir, fabCall;
 
 
     Contactos contacto;
@@ -40,6 +41,8 @@ public class VerActivity extends AppCompatActivity {
         txtNota = findViewById(R.id.txtComentario);
         fabEditar = findViewById(R.id.fabEditar);
         fabEliminar = findViewById(R.id.fabEliminar);
+        fabCompartir = findViewById(R.id.fabCompartir);
+        fabCall = findViewById(R.id.fabCall);
         btnGuarda = findViewById(R.id.btnGuarda);
         btnGuarda.setVisibility(View.INVISIBLE);
 
@@ -100,6 +103,37 @@ public class VerActivity extends AppCompatActivity {
 
                             }
                         }).show();
+            }
+        });
+
+        fabCompartir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Escoje el medio por el cuale deseas compartir el contacto.");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+            }
+        });
+
+        fabCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String num = txtTelefono.getText().toString(); // Guardamos el numero de telefono en un string
+                if(num!="") { // Verificamos si el numero de telefono no esta vacio
+                    Uri number = Uri.parse("tel:" + num); // Creamos una uri con el numero de telefono
+                    Intent dial = new Intent(Intent.ACTION_DIAL, number); // Creamos una llamada al Intent de llamadas
+                    startActivity(dial); // Ejecutamos el Intent
+                }else{ // Si el numero esta vacio
+                    // Mostramos una alerta de que debemos escribir un numero
+                    AlertDialog.Builder alert1 = new AlertDialog.Builder(VerActivity.this);
+                    alert1.setTitle("No hay numero"); // Titulo de la alerta
+                    alert1.setMessage("Debes escribir un Numero!"); // Contenido de la alerta
+                    alert1.show(); // mostrar alerta
+                }
             }
         });
     }
